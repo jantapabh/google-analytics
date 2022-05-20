c1469b876c815930fecb98fc2f2c7c99f7dc01ef
jantapabh <jantapa2407@gmail.com>
2022-05-21 01:40:02 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_ANALYTICS_ID: "G-1G8YM0F4GV"
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
+import "../styles/globals.css";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as ga from "../lib/googleAnalyticsScript";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouterChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteraction"
+      />
+      <Script id="google-analytics-script" strategy="afterInteraction">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
