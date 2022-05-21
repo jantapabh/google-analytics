d010f3d35b47d8fcc28dd60562a55a73098e8707
jantapabh <jantapa2407@gmail.com>
2022-05-22 00:24:11 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -18,19 +18,6 @@ function MyApp({ Component, pageProps }) {
   }, [router]);
   return (
     <>
-      {/* <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-        strategy="afterInteraction"
-      />
-      <Script id="google-analytics" strategy="afterInteraction">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
-        `}
-      </Script> */}
       <Component {...pageProps} />
     </>
   );
